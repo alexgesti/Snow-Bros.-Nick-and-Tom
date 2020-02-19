3e78704f9f8fbff9f6fad4534ce2d331be534a86
paumotta <61042875+paumotta@users.noreply.github.com>
2020-02-19 17:10:15 +0100

    Update Main.cpp

--- PRUEBA/HelloWorld/Main.cpp
+++ PRUEBA/HelloWorld/Main.cpp
@@ -6,7 +6,7 @@ int main()
 	printf("Hello World.\n"); //Alex
 	printf("Hello World from Raul.\n");
 	printf("Hello from Lauren.\n"); // Lauren.
-
+	printf("Hello World,grupoo.\n") 
 	system("pause");
 
 	return 0;
